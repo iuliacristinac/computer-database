1a7ca502c1310d3e4366df3e5a7ebe7def32b2c4
Iulia <iccaraiman@excilys.com>
2015-04-07 12:21:58 +0200

    feat(): BoneCP ThreadLocal

---
+++ src/main/java/com/excilys/exception/ConnectionException.java
@@ -0,0 +1,16 @@
+package com.excilys.exception;
+
+public class ConnectionException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ConnectionException() {}
+	
+	public ConnectionException(String message) {
+		super(message);
+	}
+	
+	public ConnectionException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
